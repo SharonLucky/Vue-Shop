9a730cea500777af559cb9d96ccbf64e12f9c3ce
Sharon <1210921659@qq.com>
2017-02-15 17:14:57 +0800

    add shopList

---
+++ shopList/js/address.js
@@ -0,0 +1,13 @@
+var vm = new Vue({
+  el: ".container",
+  data: {
+    limitNum:3,
+    addressIndex: 0,
+    addressList: [],
+    isNextFlag: false,
+    loadMoreFlag: false,
+    shippingMethod:1
+  },
+  mounted: function () {},
+  methods: {}
+})
