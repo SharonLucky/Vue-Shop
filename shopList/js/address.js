9db93558da3517e111face005dd715f6cbd935bf
Sharon <1210921659@qq.com>
2017-02-16 21:36:04 +0800

    add

--- shopList/js/address.js
+++ shopList/js/address.js
@@ -4,20 +4,35 @@ var vm = new Vue({
     limitNum:3,//limit showNumbers
     addressIndex: 0,
     addressList: [],
-    //isNextFlag: false,
+    showModal:false,
+    showNewAddress:false,
     loadMoreFlag: false,
-    shippingMethod:1//选择配送方式
+    shippingMethod:1,//选择配送方式
+    province: [],
+    name:'',
+    tel:'',
+    selected:''
   },
   mounted: function () {
     this.$nextTick(function () {
-      //this.loadingState = true;
       this.queryAddress();
     })
   },
   computed:{
     filteAddress:function(){
       return this.addressList.slice(0,this.limitNum)
-    }
+    },
+    selection:function () {
+      var selected_tow2 = [];
+        var that = this;
+        that.province.forEach(function (val) {
+          if(val.text == that.selected){
+            selected_tow2 =val.city;
+          }
+        });
+      return selected_tow2
+      }
+
   },
   methods: {
     queryAddress: function () {
@@ -46,7 +61,28 @@ var vm = new Vue({
     },
     delUserAddress:function (index) {
       var _this=this;
-      this.addressList.splice(index,1)
+      this.showModal=true;
+      this.delIndex=index;
+
+    },
+    delConfirm:function () {
+      var _this=this;
+      this.showModal=false;
+      setTimeout(function () {
+        _this.addressList.splice(this.delIndex,1)
+      },300)
+    },
+    getAddressInfo:function () {
+      this.$http.get('data/province.json').then(function (response) {
+        var res = response.data;
+        if (res.status == '1') {
+          this.province = res.province;
+        }
+      })
+    },
+    addNewAddress:function () {
+      this.showNewAddress=true;
+      this.getAddressInfo();
     }
   }
 })
